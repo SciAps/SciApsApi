44790e1faa2889c5fb0fe5e270909a9224b18109
Tom Riddle <triddle@sciaps.com>
2021-12-17 12:10:02 -0500

    Update docs

--- api/src/main/java/com/sciaps/ZAnalysisResult.java
+++ api/src/main/java/com/sciaps/ZAnalysisResult.java
@@ -3,19 +3,19 @@ package com.sciaps;
 import java.util.List;
 
 public class ZAnalysisResult {
-    public String mode;
-    public long timestamp;                  // unix timestamp of test result
+    public String mode;                     // Analysis mode used to produce this result
+    public long timestamp;                  // unix timestamp of the test result
     public long durationMs;                 // test duration in milliseconds
     public double latitude;                 // current latitude at time of test
     public double longitude;                // current latitude at time of test
-    public String base;                     //
-    public String modelName;                //
-    public List<ZChemInfo> chemistry;
-    public String firstGradeMatch;
-    public double firstGradeMatchScore;
-    public String secondGradeMatch;
-    public double secondGradeMatchScore;
-    public String thirdGradeMatch;
-    public double thirdGradeMatchScore;
-    public String gradeLibraryName;
+    public String base;                     // Base name
+    public String modelName;                // Model name
+    public List<ZChemInfo> chemistry;       // Elemental data
+    public String firstGradeMatch;          // Grade name of closest match
+    public double firstGradeMatchScore;     // Grade match score of closest match
+    public String secondGradeMatch;         // Grade name of second closest match
+    public double secondGradeMatchScore;    // Grade match score of second closest match
+    public String thirdGradeMatch;          // Grade name of third closest match
+    public double thirdGradeMatchScore;     // Grade match score of third closest match
+    public String gradeLibraryName;         // Name of grade library used
 }
