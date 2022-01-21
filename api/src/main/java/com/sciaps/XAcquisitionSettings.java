69c0bf51afec5fc71d6e1629779af60ccbfaf769
Tom Riddle <triddle@sciaps.com>
2022-01-21 16:05:50 -0500

    [NGX][SQLite] Define format for 3rd Party GetAcquisitionSettings API (#2)

---
+++ api/src/main/java/com/sciaps/XAcquisitionSettings.java
@@ -0,0 +1,7 @@
+package com.sciaps;
+
+import java.util.List;
+
+public class XAcquisitionSettings {
+    public List<XBeamSettings> beams;
+}
---
+++ api/src/main/java/com/sciaps/XBeamSettings.java
@@ -0,0 +1,12 @@
+package com.sciaps;
+
+public class XBeamSettings {
+    public String name;                 // Beam name
+    public double voltage;              // Voltage in KeV
+    public double current;              // Current in uA
+    public int filterPosition;          // 0-5 are valid values
+    public double timeMs;               // time in milliseconds
+    public double setCountRate;
+    public double lowCountRate;
+    public double maxCountRate;
+}
