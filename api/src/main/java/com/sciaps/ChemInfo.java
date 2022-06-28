844ed010f8de6822c2ecde2445c075b9866726b7
Tom Riddle <triddle@sciaps.com>
2022-06-28 14:20:31 -0400

    Add X Result data structures

---
+++ api/src/main/java/com/sciaps/ChemInfo.java
@@ -0,0 +1,16 @@
+package com.sciaps;
+
+public class ChemInfo {
+    public int atomicNumber;
+    public double percent;
+    public double uncertainty;
+    public int flags;
+
+    // Flags is a bitmask that with the following bits defined:
+    public static final int TYPE_ERROR      = (1 << 0); // 1
+    public static final int TYPE_NOMINAL    = (1 << 1); // 2
+    public static final int TYPE_LESS_LOD   = (1 << 3); // 8
+    public static final int TYPE_LE         = (1 << 5); // 32
+    public static final int TYPE_OCR_MINUS  = (1 << 6); // 64
+    public static final int TYPE_OCR_PLUS   = (1 << 7); // 128
+}
---
+++ api/src/main/java/com/sciaps/XTestResult.java
@@ -0,0 +1,11 @@
+package com.sciaps;
+
+import java.util.List;
+
+class XTestResult {
+   public String status;
+   public boolean abortedByUser;
+   public int errorCode;
+   public List<XSpectrum> spectra;
+   public AnalysisResult testData;
+}
