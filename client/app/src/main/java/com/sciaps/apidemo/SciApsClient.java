0af69b5ab1be0ce0d19f9727381f5330b3e77f07
Tom Riddle <triddle@sciaps.com>
2022-06-28 18:48:02 -0400

    Add X Api calls

--- client/app/src/main/java/com/sciaps/apidemo/SciApsClient.java
+++ client/app/src/main/java/com/sciaps/apidemo/SciApsClient.java
@@ -4,6 +4,13 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
 import com.sciaps.InstrumentId;
+import com.sciaps.XAcquisitionResult;
+import com.sciaps.XAcquisitionSettings;
+import com.sciaps.XCalibration;
+import com.sciaps.XFactoryAcquisitionSettings;
+import com.sciaps.XInstrumentConfig;
+import com.sciaps.XInstrumentStatus;
+import com.sciaps.XTestResult;
 import com.sciaps.ZAcquisitionResult;
 import com.sciaps.ZAcquisitionSettings;
 import com.sciaps.ZCalibration;
@@ -158,7 +165,7 @@ public class SciApsClient {
         return retval;
     }
 
-    public void runWlCalibration() {
+     public void runWlCalibration() {
         HttpPost request = new HttpPost(getUrlForEndpoint("wlcalibration"));
         try (CloseableHttpResponse response = mHttpClient.execute(request)) {
             int status = response.getStatusLine().getStatusCode();
@@ -207,7 +214,7 @@ public class SciApsClient {
         }
     }
 
-    public ZTestResult runTest(final String mode, ZAcquisitionSettings settings, boolean allSpectra) {
+    public ZTestResult runZTest(final String mode, ZAcquisitionSettings settings, boolean allSpectra) {
         ZTestResult retval = null;
         HttpPost request = new HttpPost(getUrlForEndpointWithModeAndSpecta("test", mode, allSpectra));
         String jsonStr = mGson.toJson(settings);
@@ -224,10 +231,10 @@ public class SciApsClient {
 
                 EntityUtils.consume(rspEntity);
             }
-            Main.LOGGER.info("runTest completed with status: " + status);
+            Main.LOGGER.info("runZTest completed with status: " + status);
         } catch (Exception e) {
             e.printStackTrace();
-            Main.LOGGER.info("runTest failed", e);
+            Main.LOGGER.info("runZTest failed", e);
         }
         return retval;
     }
@@ -245,10 +252,10 @@ public class SciApsClient {
 
                 EntityUtils.consume(entity);
             }
-            Main.LOGGER.info("getZAcquisitionSettings completed with status: {}", status);
+            Main.LOGGER.info("getZFactoryAcquisitionSettings completed with status: {}", status);
         } catch (Exception e) {
             e.printStackTrace();
-            Main.LOGGER.error("getZAcquisitionSettings failed", e);
+            Main.LOGGER.error("getZFactoryAcquisitionSettings failed", e);
         }
 
         return retval;
@@ -263,14 +270,14 @@ public class SciApsClient {
 
         try (CloseableHttpResponse response = mHttpClient.execute(request)) {
             int status = response.getStatusLine().getStatusCode();
-            Main.LOGGER.info("setZAcquisitionSettings completed with status: {}", status);
+            Main.LOGGER.info("setZFactoryAcquisitionSettings completed with status: {}", status);
         } catch (Exception e) {
             e.printStackTrace();
-            Main.LOGGER.error("setZAcquisitionSettings failed", e);
+            Main.LOGGER.error("setZFactoryAcquisitionSettings failed", e);
         }
     }
 
-    public ZAcquisitionResult runAcquisition(final String mode, ZFactoryAcquisitionSettings settings, boolean allSpectra) {
+    public ZAcquisitionResult runZAcquisition(final String mode, ZFactoryAcquisitionSettings settings, boolean allSpectra) {
         ZAcquisitionResult retval = null;
         HttpPost request = new HttpPost(getUrlForEndpointWithModeAndSpecta("acquire", mode, allSpectra));
         String jsonStr = mGson.toJson(settings);
@@ -287,10 +294,10 @@ public class SciApsClient {
 
                 EntityUtils.consume(rspEntity);
             }
-            Main.LOGGER.info("runAcquisition completed with status: " + status);
+            Main.LOGGER.info("runZAcquisition completed with status: " + status);
         } catch (Exception e) {
             e.printStackTrace();
-            Main.LOGGER.info("runAcquisition failed", e);
+            Main.LOGGER.info("runZAcquisition failed", e);
         }
         return retval;
     }
@@ -305,5 +312,204 @@ public class SciApsClient {
             e.printStackTrace();
             Main.LOGGER.error("abort failed", e);
         }
+    }
+
+    /***************************************/
+
+    public XInstrumentConfig getXInstrumentConfig() {
+        XInstrumentConfig retval = null;
+        HttpGet request = new HttpGet(getUrlForEndpoint("config"));
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            HttpEntity entity = response.getEntity();
+
+            JsonReader reader = new JsonReader(new InputStreamReader(entity.getContent()));
+            retval = mGson.fromJson(reader, XInstrumentConfig.class);
+
+            EntityUtils.consume(entity);
+            Main.LOGGER.info("getXInstrumentConfig completed with status: {}", status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.error("getXInstrumentConfig failed", e);
+        }
+
+        return retval;
+    }
+
+    public XInstrumentStatus getXInstrumentStatus() {
+        XInstrumentStatus retval = null;
+        HttpGet request = new HttpGet(getUrlForEndpoint("status"));
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            HttpEntity entity = response.getEntity();
+
+            JsonReader reader = new JsonReader(new InputStreamReader(entity.getContent()));
+            retval = mGson.fromJson(reader, XInstrumentStatus.class);
+
+            EntityUtils.consume(entity);
+            Main.LOGGER.info("getXInstrumentStatus completed with status: {}", status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.error("getXInstrumentStatus failed", e);
+        }
+
+        return retval;
+    }
+
+    public XCalibration getXCalibration() {
+        XCalibration retval = null;
+        HttpGet request = new HttpGet(getUrlForEndpoint("energyCal"));
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            HttpEntity entity = response.getEntity();
+
+            JsonReader reader = new JsonReader(new InputStreamReader(entity.getContent()));
+            retval = mGson.fromJson(reader, XCalibration.class);
+
+            EntityUtils.consume(entity);
+            Main.LOGGER.info("getXCalibration completed with status: {}", status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.error("getXCalibration failed", e);
+        }
+
+        return retval;
+    }
+
+    public void runEnergyCalibration() {
+        HttpPost request = new HttpPost(getUrlForEndpoint("energyCal"));
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            Main.LOGGER.info("runEnergyCalibration completed with status: " + status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.info("runEnergyCalibration failed " + e);
+        }
+    }
+
+    public XAcquisitionSettings getXAcquisitionSettings(final String mode) {
+        XAcquisitionSettings retval = null;
+        HttpGet request = new HttpGet(getUrlForEndpointWithMode("acquisitionParams/user", mode));
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            if (status == HttpStatus.SC_OK) {
+                HttpEntity entity = response.getEntity();
+
+                JsonReader reader = new JsonReader(new InputStreamReader(entity.getContent()));
+                retval = mGson.fromJson(reader, XAcquisitionSettings.class);
+
+                EntityUtils.consume(entity);
+            }
+            Main.LOGGER.info("getXAcquisitionSettings completed with status: {}", status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.error("getXAcquisitionSettings failed", e);
+        }
+
+        return retval;
+    }
+
+    public void setXAcquisitionSettings(final String mode, XAcquisitionSettings settings) {
+        HttpPut request = new HttpPut(getUrlForEndpointWithMode("acquisitionParams/user", mode));
+
+        String jsonStr = mGson.toJson(settings);
+        StringEntity entity = new StringEntity(jsonStr, ContentType.APPLICATION_JSON);
+        request.setEntity(entity);
+
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            Main.LOGGER.info("setXAcquisitionSettings completed with status: {}", status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.error("setXAcquisitionSettings failed", e);
+        }
+    }
+
+    public XTestResult runXTest(final String mode, XAcquisitionSettings settings, boolean allSpectra) {
+        XTestResult retval = null;
+        HttpPost request = new HttpPost(getUrlForEndpointWithModeAndSpecta("test", mode, allSpectra));
+        String jsonStr = mGson.toJson(settings);
+        StringEntity reqEntity = new StringEntity(jsonStr, ContentType.APPLICATION_JSON);
+        request.setEntity(reqEntity);
+
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            if (status == HttpStatus.SC_OK) {
+                HttpEntity rspEntity = response.getEntity();
+
+                JsonReader reader = new JsonReader(new InputStreamReader(rspEntity.getContent()));
+                retval = mGson.fromJson(reader, XTestResult.class);
+
+                EntityUtils.consume(rspEntity);
+            }
+            Main.LOGGER.info("runTest completed with status: " + status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.info("runTest failed", e);
+        }
+        return retval;
+    }
+
+    public XFactoryAcquisitionSettings getXFactoryAcquisitionSettings(final String mode) {
+        XFactoryAcquisitionSettings retval = null;
+        HttpGet request = new HttpGet(getUrlForEndpointWithMode("acquisitionParams/factory", mode));
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            if (status == HttpStatus.SC_OK) {
+                HttpEntity entity = response.getEntity();
+
+                JsonReader reader = new JsonReader(new InputStreamReader(entity.getContent()));
+                retval = mGson.fromJson(reader, XFactoryAcquisitionSettings.class);
+
+                EntityUtils.consume(entity);
+            }
+            Main.LOGGER.info("getXFactoryAcquisitionSettings completed with status: {}", status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.error("getXFactoryAcquisitionSettings failed", e);
+        }
+
+        return retval;
+    }
+
+    public void setXFactoryAcquisitionSettings(final String mode, XFactoryAcquisitionSettings settings) {
+        HttpPut request = new HttpPut(getUrlForEndpointWithMode("acquisitionParams/factory", mode));
+
+        String jsonStr = mGson.toJson(settings);
+        StringEntity entity = new StringEntity(jsonStr, ContentType.APPLICATION_JSON);
+        request.setEntity(entity);
+
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            Main.LOGGER.info("setXFactoryAcquisitionSettings completed with status: {}", status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.error("setXFactoryAcquisitionSettings failed", e);
+        }
+    }
+
+    public XAcquisitionResult runXAcquisition(final String mode, XFactoryAcquisitionSettings settings, boolean allSpectra) {
+        XAcquisitionResult retval = null;
+        HttpPost request = new HttpPost(getUrlForEndpointWithModeAndSpecta("acquire", mode, allSpectra));
+        String jsonStr = mGson.toJson(settings);
+        StringEntity reqEntity = new StringEntity(jsonStr, ContentType.APPLICATION_JSON);
+        request.setEntity(reqEntity);
+
+        try (CloseableHttpResponse response = mHttpClient.execute(request)) {
+            int status = response.getStatusLine().getStatusCode();
+            if (status == HttpStatus.SC_OK) {
+                HttpEntity rspEntity = response.getEntity();
+
+                JsonReader reader = new JsonReader(new InputStreamReader(rspEntity.getContent()));
+                retval = mGson.fromJson(reader, XAcquisitionResult.class);
+
+                EntityUtils.consume(rspEntity);
+            }
+            Main.LOGGER.info("runXAcquisition completed with status: " + status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.LOGGER.info("runXAcquisition failed", e);
+        }
+        return retval;
     }
 }
