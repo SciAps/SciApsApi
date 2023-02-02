6f9eb1c38f264f01948b54f4d52f406a43fe8413
Elena Loukhnova <eloukhnova@sciaps.com>
2023-02-02 03:43:20 +0300

    [SCI-13240] [SCI-13240] [NGL][PB] Add support for Spectrometer Specific Drift Correction config file (#7)
    Changes:
     - deps updated
    
    Co-authored-by: Katerina Doudkin <49165199+kdoudkinsciaps@users.noreply.github.com>

--- api/src/main/java/com/sciaps/ZInstrumentConfig.java
+++ api/src/main/java/com/sciaps/ZInstrumentConfig.java
@@ -3,7 +3,7 @@ package com.sciaps;
 import java.util.List;
 
 public class ZInstrumentConfig {
-    public List<String> spectrometers;      // Installed spectrometers
+    public String spectrometers;      // Installed spectrometers
     public boolean isArgonCapable;          // is device equipped with argon tank
     public boolean isAirPumpCapable;        // is device equipped with air pump
 }
