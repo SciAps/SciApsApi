78f413ff8143ea8fee5fcfcd88318dd2a5da38e4
Tom Riddle <triddle@sciaps.com>
2024-07-31 17:34:22 -0400

    [SCI-16504] Fix misnamed field (#12)

--- api/src/main/java/com/sciaps/XInstrumentConfig.java
+++ api/src/main/java/com/sciaps/XInstrumentConfig.java
@@ -4,6 +4,7 @@ public class XInstrumentConfig {
     public boolean isShutterInstalled;          // is device equipped a shutter
     public boolean isFilterWheelInstalled;      // is device equipped with a filter wheel
     public String detectorType;                 // DPP type: Amptek / Ketek
-    public String tubeType;                     // Tube type: Rh / Ag / Au / W / Ta
+    public String tubeAnodeType;                // Tube anode type: Rh / Ag / Au / W / Ta
+    public int tubeType;                        // Tube type: 1: 4W, 2: 5W, 3: Turbo, 4: 55KV, 5: 80KV
     public String dppVersion;                   // DPP firmware version
 }
