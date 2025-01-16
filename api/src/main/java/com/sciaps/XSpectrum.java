3baebc6e150c909b8d40287e2aca7c287decd456
Tom Riddle <triddle@sciaps.com>
2025-01-16 16:54:44 -0500

    XRF side changes

--- api/src/main/java/com/sciaps/XSpectrum.java
+++ api/src/main/java/com/sciaps/XSpectrum.java
@@ -1,20 +1,22 @@
 package com.sciaps;
 
 public class XSpectrum {
-   public int index;                         // spectrum index
-   public String beamName;                   // Beam name
-   public double energySlope;                // Energy calibration slope
-   public double energyOffset;               // Energy calibration offset
-   public double liveTime;                   // livetime
-   public double realTime;                   // realtime
-   public double liveTimeMultiplier;
-   public int outputEvents;
-   public int fastPeaks;
-   public double keV;
-   public double uA;
-   public int filterPos;
-   public double tubeTemp;
-   public double detectorTemp;
-   public int numBins;
-   public double[] data;
+    public int index;                         // spectrum index
+    public String beamName;                   // Beam name
+    public double energySlope;                // Energy calibration slope
+    public double energyOffset;               // Energy calibration offset
+    public double liveTime;                   // livetime
+    public double realTime;                   // realtime
+    public double liveTimeMultiplier;
+    public int outputEvents;
+    public int fastPeaks;
+    public double keV;
+    public double uA;
+    public int filterPos;
+    public double tubeTemp;
+    public double detectorTemp;
+    public int numBins;
+    public double[] data;       // Either data or x and y will be populated
+    public double[] x;
+    public double[] y;
 }
